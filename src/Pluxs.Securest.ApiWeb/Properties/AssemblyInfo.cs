7dc26084dfc1bf7a68e0b52e3b1977968ded0e9f
Shiny Zhu <zhushiny@gmail.com>
2012-09-19 15:52:00 +0800

    Update project info

--- src/Pluxs.Securest.ApiWeb/Properties/AssemblyInfo.cs
+++ src/Pluxs.Securest.ApiWeb/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Pluxs.Securest.ApiWeb")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple signature based REST service with ASP.NET MVC")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Pluxs")]
 [assembly: AssemblyProduct("Pluxs.Securest.ApiWeb")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Copyright © Pluxs 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
